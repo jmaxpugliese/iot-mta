e77d91504dde02d6cd3745cb93748583e8728a31
Max Pugliese <jmpuglie@us.ibm.com>
2019-02-23 16:44:25 -0500

    commented out code thats not ready

--- db_cleanup.py
+++ db_cleanup.py
@@ -10,10 +10,10 @@ class DbCleanup(threading.Thread):
     # run cleanup every 120 seconds
     while True:
       print("Cleanup database: %s" % time.ctime())
-      delete_stale_data()
+      # delete_stale_data()
       time.sleep(120)
 
-  def delete_stale_data(self):
+  # def delete_stale_data(self):
     # conntect to database
 
     # delete data with timestamp prior to current time - 120s
