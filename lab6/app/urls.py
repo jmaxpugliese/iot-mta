3c2106892818f3109726c4826dc0d9feaa8903ff
Max Pugliese <jmpuglie@us.ibm.com>
2019-03-24 16:52:24 -0400

    got directions

--- lab6/app/urls.py
+++ lab6/app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add_location/', views.add_loc, name='add_loc'),
     # ex: /app/New%20York/
-    url(r'^(.*)/$', views.temp, name='temp'),
+    # url(r'^(.*)/$', views.temp, name='temp'),
+    url(r'source/(.*)/$', views.source, name='source'),
+    url(r'source/(.*)/destination/(.*)$', views.route, name='route'),
 ]
--- lab6/app/views.py
+++ lab6/app/views.py
@@ -31,6 +31,39 @@ def temp(request, location):
 
   return render(request, 'app/temp.html', context)
 
+def source(request, src):
+  locations = Location.objects.exclude(name=src)
+  context = {
+    'source': src,
+    'locations': locations
+  }
+  return render(request, 'app/source.html', context)
+
+def route(request, src, dest):
+  src_obj = Location.objects.get(name=src)
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=52e9eafa3084f916d19f589607f5aff6'
+  src_weather = requests.get(url.format(src)).json()
+  src_obj.temperature = src_weather['main']['temp']
+  src_obj.save()
+
+  dest_obj = Location.objects.get(name=dest)
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=52e9eafa3084f916d19f589607f5aff6'
+  dest_weather = requests.get(url.format(dest)).json()
+  dest_obj.temperature = dest_weather['main']['temp']
+  dest_obj.save()
+
+  context = {
+    "src": src,
+    "src_temperature": src_weather['main']['temp'],
+    "src_icon": src_weather['weather'][0]['icon'],
+    "dest": dest,
+    "dest_temperature": dest_weather['main']['temp'],
+    "dest_icon": dest_weather['weather'][0]['icon']
+  }
+
+  return render(request, 'app/route.html', context)
+
+
 def add_loc(request):
   # If this is a POST request then process the Form data
   if request.method == 'POST':
